94ec9868a968fcb35abed5ae77b77227c32231e3
markbotond <36936610+markbotond@users.noreply.github.com>
2019-05-11 22:08:53 +0200

    Update 22.py

--- 22.py
+++ 22.py
@@ -5,9 +5,9 @@
 #palindrom számot amely előáll két 4-jegyű szám szorzataként.
 #Legnagyobb palindrom szám: 99000099, 2db 4 számjegyű szorzat: 9999 * 9901
 
-highestPalindrom = -1
-highestPalindromX = -1
-highestPalindromY = -1
+highestPalindrom = 1
+highestPalindromX = 1
+highestPalindromY = 1
 for i in range(9999,999,-1):
     for j in range(9999,999,-1):
         multply = i*j
