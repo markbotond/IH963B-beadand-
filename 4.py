c62b75700d97ddb06b6d688d97a2de78a690d7c0
markbotond <36936610+markbotond@users.noreply.github.com>
2019-05-11 20:23:09 +0200

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,3 +1,10 @@
+#4. Feladat
+#Írjon függvényt, amely paraméterként megkapja egy érvényes dátum három egész
+#értékű komponensét: egy évszámot, egy hónapszámot és egy napszámot, ebben a
+#sorrendben! A függvény visszatérési értéke legyen az az egész szám, amely jelzi, hogy
+#mennyit kell még aludni a Télapó következő látogatásáig (1 alvás/éjszaka értékkel
+#számolva és a napközbeni alvásoktól eltekintve)!
+
 from datetime import date
 def szamolo(year,month,day):
     ma = date(year,month,day)
@@ -12,4 +19,4 @@ def szamolo(year,month,day):
         delta = mikulas - ma
         return delta.days
 
-print(szamolo(2018,12,2))
+print(szamolo(2018,12,1))
