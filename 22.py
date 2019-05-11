fbc318e3f13005338846355757722d63bf30b35d
markbotond <36936610+markbotond@users.noreply.github.com>
2019-05-11 21:16:30 +0200

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,8 +1,15 @@
+#22. Feladat
+#Egy palindrom szám olyan, hogy mindkét végéről kezdve olvasva a számot, ugyan azt
+#az értéket kapjuk. A legnagyobb palindrom szám, amely előáll két 2-számjegyű szám
+#szorzataként az a 9009 = 91 × 99. Írjon programot, amely megkeresi a legnagyobb
+#palindrom számot amely előáll két 4-jegyű szám szorzataként.
+#Legnagyobb palindrom szám: 99000099, 2db 4 számjegyű szorzat: 9999 * 9901
+
 highestPalindrom = -1
 highestPalindromX = -1
 highestPalindromY = -1
-for i in range(9999,1000,-1):
-    for j in range(9999,1000,-1):
+for i in range(9999,999,-1):
+    for j in range(9999,999,-1):
         multply = i*j
         stringofmultiply = str(multply)
         if len(stringofmultiply) % 2 == 0:
