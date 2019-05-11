e2dd3ea1059cc92420ee662a62d10e5e535c86d8
markbotond <36936610+markbotond@users.noreply.github.com>
2019-05-11 22:05:20 +0200

    Update 22.py

--- 22.py
+++ 22.py
@@ -12,14 +12,13 @@ for i in range(9999,999,-1):
     for j in range(9999,999,-1):
         multply = i*j
         stringofmultiply = str(multply)
-        if len(stringofmultiply) % 2 == 0:
-            leftside = stringofmultiply[0:4]
-            rightside = stringofmultiply[4:8]
-            reverserightside = rightside[::-1]
+        leftside = stringofmultiply[0:4]
+        rightside = stringofmultiply[4:8]
+        reverserightside = rightside[::-1]
 
-            if (leftside == reverserightside) and multply > highestPalindrom:
-                highestPalindrom = multply
-                highestPalindromX = i
-                highestPalindromY = j
+        if (leftside == reverserightside) and multply > highestPalindrom:
+            highestPalindrom = multply
+            highestPalindromX = i
+            highestPalindromY = j
 
-print("legnagyobb palindrom:",highestPalindrom,"szorzótényezők",highestPalindromX,highestPalindromY)
+print("legnagyobb palindrom:",highestPalindrom,"szorzótényezők:",highestPalindromX,"*",highestPalindromY)
